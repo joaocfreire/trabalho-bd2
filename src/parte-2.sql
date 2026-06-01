8602bf36c9cf5f21ba67dab57a919c9ae2f8eb2e
João Freire <joao.campos105115@gmail.com>
2026-05-31 21:06:23 -0300

    feat: solução parcial da regra semântica da invoice/invoiceline

--- src/parte-2.sql
+++ src/parte-2.sql
@@ -89,3 +89,68 @@ WHERE customerid = 1;
 UPDATE customer
 SET supportrepid = 4 -- id de um Sales Support Agent
 WHERE customerid = 1;
+
+
+-- 4 - A base original do Chinook possui uma coluna Total na tabela Invoice representada
+-- de forma redundante com as informações contidas nas colunas UnitPrice e
+-- Quantity na tabela InvoiceLine. Podemos identificar nesse caso uma regra
+-- semântica onde o valor Total de um Invoice deve ser igual à soma de UnitPrice *
+-- Quantity de todos os registros de InvoiceLine relacionados a um Invoice.
+-- Implementar uma solução que garanta a integridade dessa regra.
+
+
+-- como a invoice deve ser criada antes das invoicelines, precisamos garantir que o total inicial é 0
+CREATE OR REPLACE TRIGGER bi_invoice_zero BEFORE INSERT ON invoice
+    FOR EACH ROW
+    BEGIN
+        :NEW.total := 0;
+    END;
+
+
+-- TODO: a trigger deve bloquear apenas a atualização manual do total, mas permitir a atualização pela aiud_invoiceline
+CREATE OR REPLACE TRIGGER bu_invoice BEFORE UPDATE of total ON INVOICE
+    FOR EACH ROW
+    BEGIN
+        IF :NEW.total != :OLD.total THEN
+            RAISE_APPLICATION_ERROR(-20103, 'ERRO! Impossível alterar o total manualmente');
+        END IF;
+    END;
+
+
+-- atualiza a consistencia do total em invoice
+CREATE OR REPLACE TRIGGER aiud_invoiceline AFTER INSERT OR UPDATE OF quantity, unitprice OR DELETE ON invoiceline
+    FOR EACH ROW
+    BEGIN
+        IF :OLD.invoicelineid IS NULL THEN -- INSERT
+            UPDATE invoice
+            SET total = total + (:NEW.unitprice * :NEW.quantity)
+            WHERE invoiceid = :NEW.invoiceid;
+
+        ELSIF :NEW.invoicelineid IS NULL THEN -- DELETE
+            UPDATE invoice
+            SET total = total - (:OLD.unitprice * :OLD.quantity)
+            WHERE invoiceid = :OLD.invoiceid;
+
+        ELSE -- se não é INSERT nem DELETE é UPDATE
+            UPDATE invoice
+            SET total = total - (:OLD.unitprice * :OLD.quantity) + (:NEW.unitprice * :NEW.quantity)
+            WHERE invoiceid = :NEW.invoiceid;
+        END IF;
+    END;
+
+-- *************** TESTES *************** --
+
+-- tenta atualizar invoice com um valor de total diferente da soma dos unitprices (1.98) - (deve FALHAR)
+UPDATE invoice
+SET total = 2.0
+WHERE invoiceid = 1;
+
+INSERT INTO invoice (invoiceid, customerid, invoicedate, total)
+    VALUES (413, 1, '31-05-2026', 0);
+
+INSERT INTO invoiceline (invoicelineid, invoiceid, trackid, unitprice, quantity)
+VALUES (2241, 413, 1, 1, 10);
+
+UPDATE invoice
+SET total = 20
+WHERE invoiceid = 413;
